af9eb54ffe297803fd5193d808f4d65d39ef955e
Firebase yuji <yuji.10473@gmail.com>
2025-11-21 22:59:33 +0000

    メタタグも必要？ <meta name="google-adsense-account" content="ca-pub-71488940793

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7148894079314433" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Literata:opsz,wght@24..144,400;700&display=swap" rel="stylesheet" />
