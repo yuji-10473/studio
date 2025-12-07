ca718e0ee76c2702cf5af0cfa4fed5088eee3edd
Firebase yuji <yuji.10473@gmail.com>
2025-12-07 11:52:43 +0000

    広告枠に 「Slack参加申請（Link: https://docs.google.com/forms/d/e/1FAIpQLSfWam1d54

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -3,10 +3,14 @@
 export default function AdBanner() {
   return (
     <div className="my-4 w-full h-[45px] bg-card/60 rounded-lg border border-border flex justify-center items-center">
-      {/* Fallback content in case ad doesn't load */}
-      <div className="text-center text-muted-foreground">
-        <p className="text-xs">広告</p>
-      </div>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSfWam1d54AoHKXY8PsPKZJmi6zhl9D9OXOG6objyPFI46Arxg/viewform?usp=header"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm font-medium text-primary hover:underline"
+      >
+        Slack参加申請
+      </a>
     </div>
   );
 }
